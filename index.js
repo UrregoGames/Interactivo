f93fcffec71657c066f13a750dc08a5c8a3a8651
Ricardo11928 <ricardovalencia1192@gmail.com>
2024-04-21 17:58:28 -0500

    Edicion de la parte de periodico

--- index.js
+++ index.js
@@ -15,26 +15,3 @@ document.getElementById('orange-container-right-5').addEventListener('click', fu
     window.location.href = 'narrativaDiver.html';
 });
 
-document.getElementById('orange-container-left-8').addEventListener('click', function() {
-    // Obtener la posición vertical de la sección 3
-    const section9 = document.getElementById('section9');
-    const section9Position = section9.offsetTop;
-
-    // Desplazarse suavemente a la sección 3
-    window.scrollTo({
-        top: section9Position,
-        behavior: 'smooth'
-    });
-});
-
-document.getElementById('orange-container-right-8').addEventListener('click', function() {
-    // Obtener la posición vertical de la sección 3
-    const section10 = document.getElementById('section10');
-    const section10Position = section10.offsetTop;
-
-    // Desplazarse suavemente a la sección 3
-    window.scrollTo({
-        top: section10Position,
-        behavior: 'smooth'
-    });
-});
