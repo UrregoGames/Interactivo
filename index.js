66ccb6fa6c11b597b013d8726945040f494f9f1c
StevenEscobar14 <stevenescobaraw@gmail.com>
2024-04-21 12:33:25 -0500

    Add: section 5 y 8

--- index.js
+++ index.js
@@ -13,12 +13,60 @@ document.getElementById('orange-container-left').addEventListener('click', funct
 
 document.getElementById('orange-container-right').addEventListener('click', function() {
     // Obtener la posición vertical de la sección 3
-    const section3 = document.getElementById('section4');
-    const section3Position = section3.offsetTop;
+    const section4 = document.getElementById('section4');
+    const section4Position = section4.offsetTop;
 
     // Desplazarse suavemente a la sección 3
     window.scrollTo({
-        top: section3Position,
+        top: section4Position,
+        behavior: 'smooth'
+    });
+});
+
+document.getElementById('orange-container-left-5').addEventListener('click', function() {
+    // Obtener la posición vertical de la sección 3
+    const section6 = document.getElementById('section6');
+    const section6Position = section6.offsetTop;
+
+    // Desplazarse suavemente a la sección 3
+    window.scrollTo({
+        top: section6Position,
+        behavior: 'smooth'
+    });
+});
+
+document.getElementById('orange-container-right-5').addEventListener('click', function() {
+    // Obtener la posición vertical de la sección 3
+    const section7 = document.getElementById('section7');
+    const section7Position = section7.offsetTop;
+
+    // Desplazarse suavemente a la sección 3
+    window.scrollTo({
+        top: section7Position,
+        behavior: 'smooth'
+    });
+});
+
+document.getElementById('orange-container-left-8').addEventListener('click', function() {
+    // Obtener la posición vertical de la sección 3
+    const section9 = document.getElementById('section9');
+    const section9Position = section9.offsetTop;
+
+    // Desplazarse suavemente a la sección 3
+    window.scrollTo({
+        top: section9Position,
+        behavior: 'smooth'
+    });
+});
+
+document.getElementById('orange-container-right-8').addEventListener('click', function() {
+    // Obtener la posición vertical de la sección 3
+    const section10 = document.getElementById('section10');
+    const section10Position = section10.offsetTop;
+
+    // Desplazarse suavemente a la sección 3
+    window.scrollTo({
+        top: section10Position,
         behavior: 'smooth'
     });
 });
